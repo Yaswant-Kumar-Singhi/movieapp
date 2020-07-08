c2877dab37e055eb030ca0a2a94bf8f692e4226a
Yaswant-Kumar-Singhi <singhiyaswant@gmail.com>
2020-07-08 23:39:50 +0530

    Updates

---
+++ precache-manifest.7704d8aa06754dec4b3e36f82f7e3fc9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85f90f3361f06916386911ead134ac09",
+    "url": "/movieapp/index.html"
+  },
+  {
+    "revision": "f042a9afce1fe1ac0c31",
+    "url": "/movieapp/static/css/main.57fbc47d.chunk.css"
+  },
+  {
+    "revision": "dc8e60baaada9396779e",
+    "url": "/movieapp/static/js/2.6673998e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movieapp/static/js/2.6673998e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f042a9afce1fe1ac0c31",
+    "url": "/movieapp/static/js/main.b604951b.chunk.js"
+  },
+  {
+    "revision": "0d516f28c241e3c70098",
+    "url": "/movieapp/static/js/runtime-main.04b21085.js"
+  }
+]);
